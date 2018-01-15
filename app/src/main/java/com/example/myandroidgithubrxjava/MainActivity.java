a70665d5205f8ebfe032e1221445861a829fcb37
DarushDev <darushfaqiri@gmail.com>
2018-01-15 19:32:26 +0800

    bind UI and populate listview in MainActivity

--- app/src/main/java/com/example/myandroidgithubrxjava/MainActivity.java
+++ app/src/main/java/com/example/myandroidgithubrxjava/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.myandroidgithubrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
+
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GithubRepoAdapter adapter = new GithubRepoAdapter();
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.list_main_repos);
+        listView.setAdapter(adapter);
+
+        final EditText fieldUserName = findViewById(R.id.field_main_username);
+        final Button buttonSearch = findViewById(R.id.button_main_search);
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userName = fieldUserName.getText().toString();
+                if (!TextUtils.isEmpty(userName)) {
+                    getStarredRepos(userName);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
+        super.onDestroy();
+    }
+
+    private void getStarredRepos(String userName) {
+        subscription = GithubClient.getInstance()
+                .getStarredRepos(userName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<GithubRepo>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted: ");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "onError: ");
+                    }
+
+                    @Override
+                    public void onNext(List<GithubRepo> githubRepos) {
+                        Log.d(TAG, "onNext: ");
+                        adapter.setGithubRepos(githubRepos);
+                    }
+                });
     }
 }
